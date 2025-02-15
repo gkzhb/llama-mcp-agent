496bfd84cb1160a2eb54715b7bce2cdf0e0dd9d4
gkzhb <gkzhb98@gmail.com>
2025-02-16 01:32:58 +0800

    feat: chat with lancedb rag

--- chat_agent.py
+++ chat_agent.py
@@ -4,6 +4,9 @@ from llama_index.core.agent import ReActAgent
 from llama_index.llms.openai_like import OpenAILike
 from llama_index.core.llms import ChatMessage
 from llama_index.llms.siliconflow import SiliconFlow
+from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.core import VectorStoreIndex, StorageContext, Settings, Document
+from llama_index.vector_stores.lancedb import LanceDBVectorStore
 import logging
 import sys
 
@@ -17,6 +20,29 @@ llm = SiliconFlow(
     temperature=0.5,
     max_tokens=4096,
 )
+
+ollama_embedding = OllamaEmbedding(
+    model_name="bge-m3",
+    base_url="http://localhost:11434",
+)
+# changing the global default
+Settings.embed_model = ollama_embedding
+Settings.llm = llm
+# Initialize LanceDB vector store
+vector_store = LanceDBVectorStore(uri="./lancedb", mode="overwrite", query_type="hybrid")
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+
+# Initialize VectorStoreIndex
+index = VectorStoreIndex([], storage_context=storage_context)
+doc = Document(text='''Your documentation here''')
+
+# Insert into vector db
+index.insert(doc)
+
+# Create query engine
+chat_engine = index.as_chat_engine()
+
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
         {"role": "assistant", "content": "I'm a smart agent!"}
@@ -41,11 +67,12 @@ for message in st.session_state.messages: # Display the prior chat messages
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
+            # Generate response using both LLM and vector store
             prompts = [ChatMessage(role=msg['role'], content=msg['content']) for msg in st.session_state.messages]
-            response = llm.stream_chat(prompts)
+            response = chat_engine.stream_chat(prompts[-1].content, prompts[:-1])
             def response_generator():
                 full_response = ""
-                for chunk in response:
+                for chunk in response.chat_stream:
                     full_response += chunk.delta
                     yield chunk.delta
                 message = {"role": "assistant", "content": full_response}
